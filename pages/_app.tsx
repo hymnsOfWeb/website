c9b871c18bc5818bccde061606c5603e01e5bf5f
Sai Rohit <sairohit11.s@gmail.com>
2023-11-30 00:09:45 +0530

    google tools addition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { Nova_Mono as NovaMono, DM_Sans as DmSans } from "next/font/google";
+import Head from "next/head";
+import Script from "next/script";
 import { Global } from "@emotion/react";
 import { AppContexProvider } from "@/context";
 import Header from "@/modules/header";
@@ -40,6 +42,34 @@ export default function App({ Component, pageProps }: AppProps) {
   }, []);
   return (
     <AppContexProvider>
+      <Head>
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJ387KF9');`}</script>
+      </Head>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TJ387KF9"
+          height="0"
+          width="0"
+          style={{ visibility: "hidden", display: "none" }}
+        ></iframe>
+      </noscript>
+
+      <Script
+        strategy="afterInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-PWC9JE6P3D"
+      ></Script>
+      <Script id="ga-analaytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PWC9JE6P3D');`}
+      </Script>
       <Global
         styles={[
           globalCss,
