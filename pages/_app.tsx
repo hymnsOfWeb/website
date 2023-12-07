dc7e33bce184b0b3198c1b54e06ae38ffd3c5ca6
theGobindSingh <thesinghgobind@gmail.com>
2023-12-07 15:39:36 +0530

    fix: ga script error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,11 +43,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContexProvider>
       <Head>
-        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TJ387KF9');`}</script>
+        <script src="/gtm-script.js" async />
       </Head>
       <noscript>
         <iframe
