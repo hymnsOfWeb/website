6bee7f6bd278a69b37628232ce72d92b17888ebe
theGobindSingh <thesinghgobind@gmail.com>
2023-12-17 18:25:51 +0530

    auto deploy

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#zero downtime deployment nextjs without vercel
+
+echo "Deploy starting..."
+
+git pull
+
+npm install || exit
+
+BUILD_DIR=temp npm run build || exit
+
+if [ ! -d "temp" ]; then
+    echo '\033[31m temp Directory not exists!\033[0m'
+    exit 1
+fi
+
+rm -rf .next
+
+mv temp .next
+
+pm2 reload app --update-env
+
+echo "Deploy done."
+
+#make sure `next.config.js` it set `distDir: process.env.BUILD_DIR`
